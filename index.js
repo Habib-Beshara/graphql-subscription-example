104547124fee642f228cbd43c803dec650714518
HabibBeshara <habib@cyber-crafts.com>
2019-08-28 11:52:09 +0200

    example using express-graphql and graphql-compose

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { graphiqlExpress } = require('apollo-server-express')
-const { createServer } = require('http')
-const { execute, subscribe } = require('graphql')
+const {createServer} = require('http')
+const {execute, subscribe} = require('graphql')
 // import { PubSub } from 'graphql-subscriptions'
-const { SubscriptionServer } = require('subscriptions-transport-ws')
-const apollo  = require('./schema')
+const graphqlHTTP = require('express-graphql')
+const {SubscriptionServer} = require('subscriptions-transport-ws')
+const expressPlayground = require('graphql-playground-middleware-express')
+  .default
+const {schemaComposer} = require('graphql-compose')
+require('./schema')
 
 const PORT = 3000
-const WS_PORT = 3001
 const app = express()
+// ===========================
 
-apollo.applyMiddleware({
-  app
-})
 
-const server = createServer(app)
-apollo.installSubscriptionHandlers(server)
-server.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`)
+const subscriptionsEndpoint = `ws://localhost:${PORT}/subscriptions`
+const schema = schemaComposer.buildSchema()
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+  subscriptionsEndpoint: subscriptionsEndpoint,
+}))
+app.get('/playground', expressPlayground({endpoint: '/graphql', subscriptionEndpoint: subscriptionsEndpoint}))
+// Create listener server by wrapping express app
+const webServer = createServer(app)
+
+webServer.listen(PORT, () => {
+  console.log(`GraphQL is now running on http://localhost:${PORT}`)
+
+  // Set up the WebSocket for handling GraphQL subscriptions.
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema,
+  }, {
+    server: webServer,
+    path: '/subscriptions',
+  })
+
 })
--- schema.js
+++ schema.js
@@ -1,34 +1,61 @@
 const {ApolloServer, gql} = require('apollo-server-express')
-const resolvers = require('./resolvers')
+const {Resolver, schemaComposer} = require('graphql-compose')
+const {PubSub} = require('graphql-subscriptions')
 
-const myGraphQLSchema = gql`
-  type Subscription {
-    someSub: String!
-  }
-  
-  type Query {
-    someQuery: String!
-  }
-  
-  type Mutation {
-    someMutation: String!
-    testSub: String!
-    
-  }
-  
-  schema {
-    query: Query
-    mutation: Mutation
-    subscription: Subscription
-  }
-  
-`
+const pubsub = new PubSub()
 
-const apollo = new ApolloServer({
-  typeDefs: myGraphQLSchema,
-  resolvers: resolvers,
-  subscriptions: {
-    path: '/subscriptions'
+// Queries
+const someQueryResolver = new Resolver({
+  name: 'someQuery',
+  type: 'String!',
+  resolve: async ({source, args, context, info}) => {
+    return 'this is a test query'
+  },
+}, schemaComposer)
+
+schemaComposer.Query.addFields({'someQuery': someQueryResolver})
+
+// Mutations
+const someMutationResolver = new Resolver({
+  name: 'someMutation',
+  type: 'String!',
+  resolve: async ({source, args, context, info}) => {
+    return 'this is a test mutation'
+  },
+}, schemaComposer)
+
+const testSubResolver = new Resolver({
+  name: 'testSub',
+  type: 'String!',
+  resolve: async ({source, args, context, info}) => {
+    pubsub.publish('SOMETHING_CHANGED', 'sub test works!')
+    return 'this Works!'
+  },
+}, schemaComposer)
+
+schemaComposer.Mutation.addFields({'someMutation': someMutationResolver})
+schemaComposer.Mutation.addFields({'testSub': testSubResolver})
+
+// Subscriptions
+const someSubscriptionResolver = new Resolver({
+  name: 'someSubscription',
+  type: 'String!',
+  subscribe: ({source, args, context, info}) => {
+    return pubsub.asyncIterator('SOMETHING_CHANGED')
+  },
+  resolve: async ({source, args, context, info}) => {
+    return 'this is a test subscription'
+  },
+}, schemaComposer)
+
+schemaComposer.Subscription.addFields({
+  someSubscription: {
+    type: 'String!',
+    subscribe: () => {
+      return pubsub.asyncIterator('SOMETHING_CHANGED')
+    },
+    resolve: async ({source, args, context, info}) => {
+      return 'this is a test subscription'
+    },
   },
 })
-module.exports = apollo
